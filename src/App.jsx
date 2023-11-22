038142f4ec9433009d4d503223ce6a90da6746d4
Nikhil Thapa <nikhilthapa.skybase@gmail.com>
2023-11-22 20:29:50 +0545

    Day 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const handleHover = () => setCount(count + 1);
+function App() {
+  const [isVisible, setIsVisible] = useState(true);
+  const toggleParaView = () => setIsVisible(!isVisible);
 
-  // Edit this component
   return (
-    <div>
-      <button data-testid="button" onMouseEnter={handleHover}>
-        Hover Me
+    <>
+      <button id="toggle-btn" onClick={toggleParaView}>
+        Toggle View
       </button>
-      <h1 data-testid="count">{count}</h1>
-    </div>
+      {isVisible && (
+        <p id="my-paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quas
+          voluptas dicta quaerat iure fugit soluta libero officia dolorem quo
+          nam, voluptates obcaecati eos debitis? Sint repudiandae culpa eaque
+          optio?
+        </p>
+      )}
+    </>
   );
-};
+}
 
 export default App;
