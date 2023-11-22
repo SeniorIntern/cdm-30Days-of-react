abb116610d8d157aab6bf0d0e20d71c0f25732d6
Nikhil Thapa <nikhilthapa.skybase@gmail.com>
2023-11-22 20:26:01 +0545

    Day 1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  // Edit this component
+  return (
+    <button data-testid="button" onClick={() => console.log("Clicked!")}>
+      Click me!
+    </button>
+  );
+};
+
+export default App;
